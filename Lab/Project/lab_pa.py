0fabbc2758b7cdb3fb29d19d8b27f51ce8fa1cf1
Kenneth Irving <irvingkennet45@gmail.com>
2026-08-30 09:49:50 -0400

    Create lab_pa.py

---
+++ Lab/Project/lab_pa.py
@@ -0,0 +1,16 @@
+# Kenneth Irving (kenirv5642)
+# Date: 08-30-2026
+# Description:
+from datetime import datetime
+
+print("kenirv5642's Excel Spreadsheet Automation Menu")
+
+# The next line retrieves the inputted option and stores into the variable called 'menuOptions'.
+menuOptions = int(input(""
+    "Choose a number from the following options\n"
+    "1. Option 1\n"
+    "2. Option 2\n"
+    "3. Option 3\n"
+    ))
+
+print(f'You selected {menuOptions} at ', str(datetime.now()))
